31b9e7f37e217cc697c1a2ccbb5a68b95d8bd994
Racer <reaganjoseph26@yahoo.com>
2020-11-19 17:41:41 -0500

    ignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,7 @@ var cityInputEl = document.querySelector("#city-search") // input bar element
 var recentCityEl = document.querySelector("#recent-cities") //ul element 
 var searchButtonEl = document.querySelector("#btn")
 
-// fetch request to open weather
-var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + myKey
 
-fetch(apiUrl).then(function (response) {
-    console.log(response)
-
-    response.json().then(function (data) {
-        console.log(data)
-    })
-
-});
 
 //create a function that handles the error handling for a city search 
 // var citySearchHnadler = function (event) {
@@ -38,17 +28,60 @@ fetch(apiUrl).then(function (response) {
 
 //display recent city searched to page under recents 
 
-var displayCity = function () {
+var displayCity = function (event) {
+    event.preventDefault()
 
-    //create the li
-    var cityHolderEl = document.createElement("li") // the li to be appended to ul
-    cityHolderEl.classList = "list-group-item"; //class for li
-    cityHolderEl.textContent = cityInputEl.value // set the li textcontent = to the content placed in the input bar
+    // fetch request to open weather
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl.value + "&appid=" + myKey
 
-    //append li to ul
-    recentCityEl.appendChild(cityHolderEl);
+    fetch(apiUrl).then(function (response) {
 
-    
+        response.json().then(function (data) {
+
+            var convertedDate = (new Date(data.dt * 1000));
+
+            document.querySelector("#city-name").textContent = data.name + convertedDate.toDateString();
+            console.log(new Date(data.dt * 1000))
+
+            console.log(data.name);
+            //create the li
+            var cityHolderEl = document.createElement("li") // the li to be appended to ul
+            cityHolderEl.classList = "list-group-item"; //class for li
+            cityHolderEl.textContent = cityInputEl.value // set the li textcontent = to the content placed in the input bar
+
+            //append li to ul
+            recentCityEl.appendChild(cityHolderEl);
+
+            console.log(data)
+        })
+
+    });
+
+    var apiForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInputEl.value + "&appid=" + myKey
+    fetch(apiForecastUrl).then(function (response) {
+
+        response.json().then(function (data) {
+
+
+            var dayOneData = data.list[2]
+            var dayTwoData = data.list[10]
+
+            document.querySelector("#day1-date").textContent = (new Date(dayOneData.dt * 1000)).toDateString()
+            document.querySelector("#day1-temp").textContent = "Temp: " + Math.floor((parseInt(dayOneData.main.temp) - 273.15) * 9/5 + 32) + " F";
+
+            console.log(dayOneData)
+
+            console.log(data)
+        })
+
+    });
+
+
+
+
+
+
+
 
 }
 
